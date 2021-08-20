5acdf12ca3d0df3f0fe18dd5a0869dd709a5f42b
JiangHua <jianghua@whu.edu.cn>
2021-08-20 23:27:41 +0800

    add another print

--- ggg/ViewController.swift
+++ ggg/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         print("hello")
+        print("hello gittutoial")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
